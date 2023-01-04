35afb611bd58174e44e86278ac6e4a7ac2d6743d
Crowdbotics <team@crowdbotics.com>
2023-01-04 17:05:13 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "business-intelligen-38179",
+  url: "https://business-intelligen-38179.botics.co",
+  api: "https://business-intelligen-38179.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
